1b0633c61ae65b9681112c3d0b78cb1c749db34c
Sanketzss <119167099+Sanketzss@users.noreply.github.com>
2025-12-31 06:12:05 +0000

    Add files via upload
    1st

---
+++ NewsdbApplication.java
@@ -0,0 +1,12 @@
+package com.example.newsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsdbApplication.class, args);
+    }
+}
